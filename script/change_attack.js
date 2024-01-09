1b74cbb74d92bd055431955f2247958f0be252af
zhefeng.yf <zhefeng.yf@alibaba-inc.com>
2024-01-09 14:40:57 +0800

    feat: add script

--- script/change_attack.js
+++ script/change_attack.js
@@ -1,18 +1,43 @@
-// import path from 'path';
+const path = require("path");
+const fs = require("fs");
 
-const fs = require('fs');
+const getPath = require("./entry-path");
+const dirPath = path.resolve("./");
+const dirPathDlc = path.resolve("./dlc_dul_cru/");
+const paths = getPath(dirPath).map(pathStr => path.resolve("./", pathStr));;
+const dlcPaths = getPath(dirPathDlc).map(pathStr => path.resolve("./dlc_dul_cru/", pathStr));
+const allHeroPaths = paths.concat(dlcPaths).filter(path => path !== 'hero_rules_data_export');
+console.log(allHeroPaths);
 
-const filePath = './dlc_dul_cru/hero_cru_data_export.Group.csv';
-fs.readFile(filePath, 'utf8', (err, data) => {
+allHeroPaths.forEach((path) => {
+  // updateFileAttributes(path)
+})
+
+const DefaultHealthHeader = 'key_map,health_max,speed,stress_max,deaths_door_chance,speed_number_of_turns,health_heal_percent_between_nodes,route_choice_chance,';
+
+const updateFileAttributes = (filePath) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
       console.error(err);
       return;
     }
-    const newStr = data.replace(/key_map\,health_damage\,health_damage_range\,crit_chance\,\r?\nadd_stats\,([\.|\d]+)\,([\.|\d]+)\,([\.|\d]+)\,/g,
-        (value,damage,range,crit_chance) => {
-            return `key_map,health_damage,health_damage_range,crit_chance,\nadd_stats,${parseInt(damage)-2},${range},${crit_chance/2},`
-        }
-    )
-    fs.writeFileSync(filePath, newStr);
+    const newStr = data.replace(
+      /DefaultHealthHeader\r?\nadd_stats\,([\.|\d]+)\,([\.|\d]+)\,([\.|\d]+)\,/g,
+      (value, health_max, speed, stress_max, deaths_door_chance, speed_number_of_turns, health_heal_percent_between_nodes, route_choice_chance) => {
+        // add_stats,40,5,10,1,1,0.1,0.67,
+        return `${DefaultHealthHeader}\nadd_stats,${health_max + 4},${speed},${stress_max},${deaths_door_chance},${speed_number_of_turns},${health_heal_percent_between_nodes},${route_choice_chance},`;
+      }
+    );
+    // const newStr = data.replace(
+    //   /key_map\,health_damage\,health_damage_range\,crit_chance\,\r?\nadd_stats\,([\.|\d]+)\,([\.|\d]+)\,([\.|\d]+)\,/g,
+    //   (value, damage, range, crit_chance) => {
+    //     return `key_map,health_damage,health_damage_range,crit_chance,\nadd_stats,${
+    //       parseInt(damage) - 2
+    //     },${range},${crit_chance / 2},`;
+    //   }
+    // );
 
+    fs.writeFileSync(filePath, newStr);
   });
+}
+
